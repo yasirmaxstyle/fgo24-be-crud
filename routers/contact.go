02e41e25d54ab4bb0c7be9b7df12e92e7b607de7
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-25 08:25:55 +0700

    feat: remove trailing slash in router path

--- routers/contact.go
+++ routers/contact.go
@@ -7,9 +7,9 @@ import (
 )
 
 func contactRouter(r *gin.RouterGroup) {
-	r.GET("/", controllers.GetContacts)
+	r.GET("", controllers.GetContacts)
 	r.GET("/:id", controllers.GetContactByID)
-	r.POST("/", controllers.AddContact)
+	r.POST("", controllers.AddContact)
 	r.PATCH("/:id", controllers.UpdateContact)
 	r.DELETE("/:id", controllers.DeleteContact)
 }
