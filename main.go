22c5bbd4b3f790da39f367eff21db8cdab535525
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-25 07:25:20 +0700

    feat: add main for index program

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"dashboard-backend/routers"
+	"dashboard-backend/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	utils.InitDB()
+
+	routers.CombineRouter(r)
+
+	r.Run()
+}
