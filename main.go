74b5bd6d2be8822d8d2da57c84a57f290003ce7d
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-25 09:44:37 +0700

    feat: add middleware for allowing cross origin

--- main.go
+++ main.go
@@ -10,6 +10,19 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	utils.InitDB()
 
 	routers.CombineRouter(r)
