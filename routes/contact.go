addac95c6ea522b717ae8fc341a8d3d5fd6f5cce
yasirmaxstyle <yasirmu77@gmail.com>
2025-07-08 14:53:04 +0700

    feat: add path endpoint api for contacts

---
+++ routes/contact.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"dashboard-backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func contactRouter(r *gin.RouterGroup) {
+	r.GET("", controllers.GetContacts)
+	r.GET("/:id", controllers.GetContactByID)
+	r.POST("", controllers.AddContact)
+	r.PATCH("/:id", controllers.UpdateContact)
+	r.DELETE("/:id", controllers.DeleteContact)
+}
